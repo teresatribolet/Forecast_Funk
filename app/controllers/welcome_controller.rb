4b2467e7ae19e4b8b58e340380686dbefa9988da
Megan Bell <meganleabell@gmail.com>
2015-08-11 10:12:31 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,41 @@
+class WelcomeController < ApplicationController
+  
+   def index
+   @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC )
+    
+   @states.sort!
+
+
+    	if params[:city] != nil
+       	params[:city].gsub! " ", "_"
+    	end
+    
+    	if params[:city] != nil
+        	response = HTTParty.get("http://api.wunderground.com/api/466bbe42e5a884a1/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
+    	if response["location"] == nil
+      	redirect_to index_path   
+    	else
+       
+    	@location = response["location"]["city"]
+    	@temp_f = response["current_observation"]["temp_f"]
+    	@temp_c = response["current_observation"]["temp_c"]
+    	@weather_icon = response["current_observation"]["icon_url"]
+    	@weather_words = response["current_observation"]["weather"]
+    	@forecast_link = response["current_observation"]["forecast_url"]
+    	@real_feel = response["current_observation"]["feelslike_f"]
+  		end
+	end
+end
+  def test
+  response = HTTParty.get("http://api.wunderground.com/api/466bbe42e5a884a1/geolookup/conditions/q/TX/Dallas.json")
+  
+  @location = response['location']['city']
+  @temp_f = response['current_observation']['temp_f']
+  @temp_c = response['current_observation']['temp_c']
+  @weather_icon = response['current_observation']['icon_url']
+  @weather_words = response['current_observation']['weather'] 
+  @forecast_link = response['current_observation']['forecast_url']
+  @real_feel = response['current_observation']['feelslike_f']
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,69 @@
+
+
+  Rails.application.routes.draw do
+   # This is setting the root page to 'welcome/index'
+  root 'welcome#index'
+  # This is setting our url to 'localhost:3000/index' instead of 'localhost:3000/welcome/index'
+  get 'index' => 'welcome#index'
+  # This is allowing us to submit our city and state, in other words 'post'
+  post 'index' => 'welcome#index'
+  # This is setting our url to 'localhost:3000/test' instead of 'localhost:3000/welcome/test'
+  get 'test' => 'welcome#test'
+
+
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
